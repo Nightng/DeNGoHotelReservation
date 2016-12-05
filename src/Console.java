de225d055439ee9f7a8af30f57847cb3b23e8616
WNG <wilson_ng1ve@yahoo.com>
2016-12-04 16:50:38 -0800

    Added addCustomer, deleteCustomer, updateCustomer functionality

--- src/Console.java
+++ src/Console.java
@@ -140,19 +140,6 @@ class Command {
 			e.printStackTrace();
 		}
 	}
-	private void checkPricing() {
-		try {
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery("select * from room");
-		    while(rs.next()){
-		       System.out.println("roomID= "+rs.getString("roomID")+", "+"price= " + rs.getInt("price"));
-		    }
-		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-		}
-	}
-
 	public void execute() throws Exception {
 		
 			if(this.cmd.equals("getCustomers")){
@@ -169,9 +156,6 @@ class Command {
 			}
 			else if(this.cmd.equals("customerReservation")){
 				this.customerReservation();
-			}
-			else if( this.cmd.equals("checkPricing") ) {
-				this.checkPricing();
 			}
 			else{
 				throw new Exception("unrecognized command: " + this.cmd);
